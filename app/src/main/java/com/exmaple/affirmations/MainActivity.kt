9f6ed5452ed462a8be852b25476cce6021ecbaf0
dmartynov <denis.martynov.93@mail.ru>
2022-05-08 11:14:32 +0300

    change layout manager

--- app/src/main/java/com/exmaple/affirmations/MainActivity.kt
+++ app/src/main/java/com/exmaple/affirmations/MainActivity.kt
@@ -2,7 +2,7 @@ package com.exmaple.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.exmaple.affirmations.adapter.ItemAdapter
 import com.exmaple.affirmations.data.Datasource
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         val myDataSet = Datasource().loadAffirmations()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
         recyclerView.setHasFixedSize(true)
     }
 }
